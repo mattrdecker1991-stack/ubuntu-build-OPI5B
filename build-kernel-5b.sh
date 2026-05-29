701ac9e8c8053758ef747d0fa3b0a6b6ce868c9a
mattrdecker1991-stack <mattrdecker1991@gmail.com>
2026-05-28 23:38:40 -0400

    Add files via upload

---
+++ build-kernel-5b.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -eE
+trap 'echo Error: in $0 on line $LINENO' ERR
+
+if [ $# -ne 1 ]; then
+	echo "$0 linux_dir"
+	exit 1
+fi
+
+# ディスクイメージを作成するために必要なツールをインストール
+sudo apt-get update && sudo apt-get -y install  build-essential gcc-aarch64-linux-gnu bison \
+qemu-user-static qemu-system-arm qemu-efi-aarch64 binfmt-support \
+debootstrap flex libssl-dev bc rsync kmod cpio xz-utils fakeroot parted \
+udev dosfstools uuid-runtime git-lfs device-tree-compiler python3 \
+python-is-python3 fdisk bc debhelper python3-pyelftools python3-setuptools \
+python3-pkg-resources swig libfdt-dev libpython3-dev gawk \
+git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex \
+libelf-dev bison libgnutls28-dev libdw-dev
+
+linux_dir=$1
+
+rm -rf $linux_dir && mkdir $linux_dir
+mem_size=`free --giga|grep Mem|awk '{print $2}'`
+if [ $mem_size -gt 8 ]; then
+	sudo mount -t tmpfs -o size=8G tmpfs $linux_dir
+fi
+
+cd $linux_dir
+git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git -b linux-7.0.y
+
+cd linux
+make defconfig
+
+./scripts/kconfig/merge_config.sh -m .config ../../my-add-5b.txt
+
+./scripts/config --set-val DEBUG_INFO_NONE y
+./scripts/config --disable DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT
+./scripts/config --disable DEBUG_INFO_DWARF4
+./scripts/config --disable DEBUG_INFO_DWARF5
+
+make olddefconfig
+
+fakeroot make -j$(nproc) LOCALVERSION="-rockchip" deb-pkg
+tmp_var=$(make LOCALVERSION="-rockchip" -s kernelrelease)
+echo "tmp_var=$tmp_var" > ../../tmp_var.txt
+
+# Exit trap is no longer needed
+trap '' EXIT
+cd ..
+cp *.deb ..
+cd ..
+echo "DISK usage"
+df $1
+if [ $mem_size -gt 4 ]; then
+	sudo umount $linux_dir
+	sleep 2
+fi
+exit 0
---
+++ das-u-boot-5b.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# ディスクイメージを作成するために必要なツールをインストール
+sudo apt-get update && sudo apt-get -y install  build-essential gcc-aarch64-linux-gnu bison \
+qemu-user-static qemu-system-arm qemu-efi-aarch64 binfmt-support \
+debootstrap flex libssl-dev bc rsync kmod cpio xz-utils fakeroot parted \
+udev dosfstools uuid-runtime git-lfs device-tree-compiler python3 \
+python-is-python3 fdisk bc debhelper python3-pyelftools python3-setuptools \
+python3-pkg-resources swig libfdt-dev libpython3-dev gawk \
+git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex \
+libelf-dev bison libgnutls28-dev libdw-dev
+
+	rm -rf arm64
+	mkdir arm64
+mem_size=`free --giga|grep Mem|awk '{print $2}'`
+if [ $mem_size -gt 2 ]; then
+        sudo mount -t tmpfs -o size=1G tmpfs arm64
+fi
+	cd arm64
+
+		git clone --depth 1 https://github.com/rockchip-linux/rkbin
+		
+		DDR=`ls rkbin/bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v*.bin`
+		BL31=`ls rkbin/bin/rk35/rk3588_bl31*.elf`
+	export BL31=`pwd`/$BL31
+	export ROCKCHIP_TPL=`pwd`/$DDR
+echo ""
+echo "export ROCKCHIP_TPL=$ROCKCHIP_TPL"
+echo "export BL31=$BL31"
+echo ""
+		export PATH=`pwd`/tools/binman:$PATH
+echo ""
+echo "PATH=$PATH"
+echo ""
+
+		git clone --depth 1 https://gitlab.com/u-boot/u-boot.git -b v2026.04
+		cd u-boot
+		if [ ! -f configs/$1 ]; then
+			echo "$1 not found in configs"
+			cd ..
+			exit 1
+		fi
+
+	echo 'CONFIG_SYS_SOC="rk3588"' >> configs/$1
+
+		make clean $1
+		make -j8
+		cp u-boot-rockchip.bin ../..
+	echo "dd if=u-boot-rockchip.bin of=/dev/sdX seek=1 bs=32k conv=fsync"
+	cd ../..
+echo "DISK usage"
+df arm64
+if [ $mem_size -gt 2 ]; then
+        sudo umount arm64
+	sleep 2
+fi 
+exit 0
+
